3ad7fc6eab45adefdccd685bc5050dcaabb47759
yaniis06 <tounisyanis79@gmail.com>
2026-06-23 00:29:42 +0200

    executor: fork execcv avec redirection dup2

--- src/executor.c
+++ src/executor.c
@@ -21,3 +21,37 @@ static double	now_seconds(void)
 		return (0.0);
 	return ((double)tv.tv_sec + (double)tv.tv_usec / 1000000.0);
 }
+
+/* fork + execv du binaire resolu, avec redirection d'entree eventuelle.
+ * Dans l'enfant on utilise _exit pour ne pas re-vider les tampons stdio
+ * herites du parent (qui provoqueraient des sorties dupliquees). */
+static pid_t	spawn_process(char *full, char **argv, const char *infile)
+{
+	pid_t	pid;
+	int		fd;
+
+	pid = fork();
+	if (pid < 0)
+		return (-1);
+	if (pid == 0)
+	{
+		if (infile != NULL)
+		{
+			fd = open(infile, O_RDONLY);
+			if (fd < 0)
+			{
+				print_error();
+				_exit(1);
+			}
+			if (dup2(fd, STDIN_FILENO) < 0 || close(fd) < 0)
+			{
+				print_error();
+				_exit(1);
+			}
+		}
+		execv(full, argv);
+		print_error();
+		_exit(1);
+	}
+	return (pid);
+}
