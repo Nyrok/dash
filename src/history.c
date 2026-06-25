3356a3a63e79fd1889f080896a0f8f66e23fb722
Sbekri <bekrisalim36@gmail.com>
2026-06-25 21:35:58 +0200

    History: consommateur queue_pop avec variable de condition

--- src/history.c
+++ src/history.c
@@ -27,3 +27,24 @@ void	queue_push(const char *line)
 	pthread_cond_signal(&g_shell.queue_not_empty);
 	pthread_mutex_unlock(&g_shell.queue_mutex);
 }
+
+/* Consommateur : retire une ligne. Attend tant que la file est vide et que
+ * le shell tourne. Renvoie 0 quand il n'y a plus rien et que la file est
+ * fermee (fin de vie du thread). */
+int	queue_pop(char *out)
+{
+	pthread_mutex_lock(&g_shell.queue_mutex);
+	while (g_shell.queue.size == 0 && !g_shell.queue.closed)
+		pthread_cond_wait(&g_shell.queue_not_empty, &g_shell.queue_mutex);
+	if (g_shell.queue.size == 0 && g_shell.queue.closed)
+	{
+		pthread_mutex_unlock(&g_shell.queue_mutex);
+		return (0);
+	}
+	strncpy(out, g_shell.queue.buffer[g_shell.queue.head], HISTORY_LINE - 1);
+	out[HISTORY_LINE - 1] = '\0';
+	g_shell.queue.head = (g_shell.queue.head + 1) % QUEUE_MAX;
+	g_shell.queue.size--;
+	pthread_mutex_unlock(&g_shell.queue_mutex);
+	return (1);
+}
