dce654fc5208fd62477a3075b7c07f548cbdd3d0
Nyrok <nyrokgaming1@gmail.com>
2026-06-27 22:55:19 +0200

    test(tests): micro framework MinUnit

---
+++ tests/minunit.h
@@ -0,0 +1,25 @@
+/* MinUnit : micro-framework de tests unitaires (approche minimaliste).
+ * Voir http://www.jera.com/techinfo/jtns/jtn002.html
+ */
+#ifndef MINUNIT_H
+# define MINUNIT_H
+
+# include <stdio.h>
+
+# define mu_assert(message, test) \
+	do { \
+		if (!(test)) \
+			return (message); \
+	} while (0)
+
+# define mu_run_test(test) \
+	do { \
+		const char *message = test(); \
+		tests_run++; \
+		if (message) \
+			return (message); \
+	} while (0)
+
+extern int	tests_run;
+
+#endif
