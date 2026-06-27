09334465e2b726c90d42c591c146f6aba8fd8de4
Nyrok <nyrokgaming1@gmail.com>
2026-06-27 20:02:43 +0200

    feat: soumission d'une commande au pool

--- src/pool.c
+++ src/pool.c
@@ -19,3 +19,27 @@ void	pool_destroy(void)
 	pthread_cond_destroy(&g_shell.pool.not_empty);
 	pthread_cond_destroy(&g_shell.pool.done);
 }
+
+/* Dépose une commande à exécuter et incrémente le compteur de tâches en cours. */
+void	pool_submit(const char *command)
+{
+	pthread_mutex_lock(&g_shell.pool.mutex);
+	strncpy(g_shell.pool.tasks[g_shell.pool.tail].command, command,
+		HISTORY_LINE - 1);
+	g_shell.pool.tasks[g_shell.pool.tail].command[HISTORY_LINE - 1] = '\0';
+	g_shell.pool.tail = (g_shell.pool.tail + 1) % QUEUE_MAX;
+	g_shell.pool.size++;
+	g_shell.pool.pending++;
+	pthread_cond_signal(&g_shell.pool.not_empty);
+	pthread_mutex_unlock(&g_shell.pool.mutex);
+}
+
+/* Bloque jusqu'à ce que toutes les tâches soumises soient terminées.
+ * Sert au thread principal pour n'afficher le prompt qu'après la ligne. */
+void	pool_wait_all(void)
+{
+	pthread_mutex_lock(&g_shell.pool.mutex);
+	while (g_shell.pool.pending > 0)
+		pthread_cond_wait(&g_shell.pool.done, &g_shell.pool.mutex);
+	pthread_mutex_unlock(&g_shell.pool.mutex);
+}
